59a5a5bb84a1e259001ee40ac165105610e2e0ba
Benjamin Herr <ben@0x539.de>
2020-07-08 23:06:50 -0700

    Create database directory if it's not there

--- src/web/index.ts
+++ src/web/index.ts
@@ -1,7 +1,11 @@
+import fs from 'fs';
 import PouchDB from 'pouchdb';
 import express from 'express';
 
 async function app(): Promise<express.Express> {
+  // pouchdb doesn't deal if this directory doesn't already exist
+  fs.mkdirSync("database", { recursive: true });
+
   const handler = require('express-pouchdb')();
   await handler.setPouchDB(PouchDB.defaults({
     prefix: './database/',
